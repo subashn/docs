800f99548600d5661eac4ecae554c1c1d3c7061c
versionit <rwn1116@gmail.com>
2016-12-12 09:25:49 +0530

    Adding init.sh
    Adding init.sh

---
+++ vm-init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sed -i -e '/^SELINUX/ c SELINUX=disabled' /etc/selinux/config
+systemctl disable firewalld
+yum install wget zip unzip gzip vim -y
+####echo devops |passwd --stdin root
+sed -i -e '/^Password/ c PasswordAuthentication yes' -e '/^PermitRootLogin/ d' -e '/#PermitRootLogin/ c PermitRootLogin yes' /etc/ssh/sshd_config
+reboot
