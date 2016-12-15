b26a5b9df4285a8c93905785bb5f869f04e3d75d
versionit <rwn1116@gmail.com>
2016-12-15 08:56:45 +0530

    Update vm-init.sh
    Reverting google cloud steps

--- vm-init.sh
+++ vm-init.sh
@@ -7,11 +7,3 @@ yum install wget zip unzip gzip vim -y
 sed -i -e '/^Password/ c PasswordAuthentication yes' -e '/^PermitRootLogin/ d' -e '/#PermitRootLogin/ c PermitRootLogin yes' /etc/ssh/sshd_config
 reboot
 
-
-
-###### For Only Google Cloud, Keep the following lines in startup script.
-
-sed -i -e '/^SELINUX/ c SELINUX=disabled' /etc/selinux/config
-systemctl disable firewalld
-yum install wget zip unzip gzip net-tools -y
-reboot
