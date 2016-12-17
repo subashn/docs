eaff16ac05a583f08f7e0fa386c15a707dd62345
versionit <rwn1116@gmail.com>
2016-12-17 20:43:24 +0530

    Update vm-init.sh
    up

--- vm-init.sh
+++ vm-init.sh
@@ -2,8 +2,8 @@
 
 sed -i -e '/^SELINUX/ c SELINUX=disabled' /etc/selinux/config
 systemctl disable firewalld
-yum install wget zip unzip gzip vim -y
+yum install wget zip unzip gzip vim net-tools -y
 ####echo devops |passwd --stdin root
-sed -i -e '/^Password/ c PasswordAuthentication yes' -e '/^PermitRootLogin/ d' -e '/#PermitRootLogin/ c PermitRootLogin yes' /etc/ssh/sshd_config
+sed -i -e '/^Password/ c PasswordAuthentication yes' -e '/^PermitRootLogin/ d' -e '/#PermitRootLogin/ c PermitRootLogin yes' -e '/TCPKeepAlive/ c TCPKeepAlive yes' -e '/ClientAliveInterval/ c ClientAliveInterval 10' /etc/ssh/sshd_config
 reboot
 
