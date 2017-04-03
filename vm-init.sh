506195f7768d612b1735c8d19e22a5f2f255f44c
versionit <rwn1116@gmail.com>
2017-04-03 18:56:32 +0530

    Update vm-init.sh

--- vm-init.sh
+++ vm-init.sh
@@ -35,7 +35,7 @@ SELINUX() {
 PACK() {
 
 	Print SL "=>> Installing base Packages.. " B
-	yum install wget zip unzip gzip vim net-tools -y &>/dev/null
+	yum install wget zip unzip gzip vim net-tools facter -y &>/dev/null
 	Print NL Success G
 }
 
